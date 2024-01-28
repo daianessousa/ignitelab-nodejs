66d8a03984c62f4396b16572bc83a604ba00b7df
Daiane Sousa <daisousa.c@gmail.com>
2024-01-27 23:28:38 -0300

    remove test

--- src/app/useCases/cancelNotificationUseCase.spec.ts
+++ src/app/useCases/cancelNotificationUseCase.spec.ts
@@ -5,27 +5,6 @@ import { CancelNotification } from './cancelNotificationUseCase';
 import { NotificationNotFound } from './errors/NotificationNotFound';
 
 describe('Cancel Notification', () => {
-  it('should be able to canceled a notification', async () => {
-    const notificationsRepository = new InMemoryNotificationsRepository();
-    const cancelNotification = new CancelNotification(notificationsRepository);
-
-    const notification = new Notification({
-      category: 'social',
-      content: new Content('Nova solicitação de amizade!'),
-      recipientId: 'test-id',
-    });
-
-    notificationsRepository.create(notification);
-
-    await cancelNotification.execute({
-      notificationId: notification.id,
-    });
-
-    expect(notificationsRepository.notifications[0].canceledAt).toEqual(
-      expect.any(Date),
-    );
-  });
-
   it('should not be cancelled a notification when it does not exist', async () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
     const cancelNotification = new CancelNotification(notificationsRepository);
